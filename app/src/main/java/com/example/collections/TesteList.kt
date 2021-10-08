e2398ba1982247699191aa52464ea7b7d9ec19de
Arthur Moreira Rodrigues <a.tertulianoa@gmail.com>
2021-10-08 09:48:16 -0300

    sortby e group

--- app/src/main/java/com/example/collections/TesteList.kt
+++ app/src/main/java/com/example/collections/TesteList.kt
@@ -1,9 +1,9 @@
 package com.example.collections
 
 fun main() {
-    val joao = Funcionario(nome = "João" , salario = 1000.0)
-    val maria = Funcionario(nome = "Maria" , salario = 1500.0)
-    val jose = Funcionario(nome = "Jose" , salario = 2000.0)
+    val joao = Funcionario(nome = "João" , salario = 1500.0 , tipocontratacao = "PJ")
+    val maria = Funcionario(nome = "Maria" , salario = 1000.0 , tipocontratacao = "CLT")
+    val jose = Funcionario(nome = "Jose" , salario = 2000.0 , tipocontratacao = "PJ")
 
    val funcionarios = listOf(joao , maria , jose)
 
@@ -11,10 +11,18 @@ fun main() {
     println("---------------")
     println(funcionarios.find {it.nome == "João"})
 
+    println("---------------")
+    funcionarios.sortedBy { it.salario } //a ordenação pode ser por salario ou nomes
+        .forEach {println(it)}
+
+    println("---------------")
+    funcionarios.groupBy { it.tipocontratacao }
+        .forEach {println(it)}
 }
 data class Funcionario(
     val nome :String,
-    val salario :Double
+    val salario :Double,
+    val tipocontratacao :String
 ) {
     override fun toString(): String =
         """
